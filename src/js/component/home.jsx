349a97bc754dd417d979a3d60b109cf9afd3eb4f
Adriana Chiquito <144694058+adrianacp10@users.noreply.github.com>
2024-01-19 16:10:12 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,96 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	
+	 const [inputValue, setInputValue] = useState ("");
+	 const [saveTask, setSaveTask] = useState ([]);
+	 const handleKeyDown = (e) => {
+		if (e.keyCode === 13) {
+			setSaveTask([...saveTask, inputValue])
+			setInputValue("")
 
-export default Home;
+		}
+	 }
+	 const handleDeleteTask = (index) => {
+		setSaveTask(saveTask.filter((_, currentIndex) => index !== currentIndex));
+	 }
+		
+	 return (
+		 	<div className="container">
+		 		<h1>My to do's </h1>
+
+				<div>
+		 				<input type="text" onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown={handleKeyDown} 
+		 				placeholder="Things you need to do"></input>
+
+				</div>
+		
+		 		<ul>
+					{saveTask.map((t, index) => (
+					<li key={index}> {t} 
+		 			<i className="fas fa-trash" 
+					onClick={() => handleDeleteTask(index)}>
+					</i>
+					</li>
+					))}
+		 		</ul>
+
+				<div>{saveTask.length} tasks to do left</div>
+		 	</div>
+		)
+	};
+	
+	export default Home;
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	// const [count, setCount] = useState (0);
+	
+	// useEffect(() => {
+		// 	// Actualiza el título del documento usando la API del navegador
+		// 	document.title = `You clicked ${count} times`;
+		//   });
+		
+		// return (
+			// 	<div>
+			// 		<p> Has hecho click {count} veces</p>
+	// 		<button onClick={() => setCount(count + 1)}>
+	// 			Hazme click!
+	// 		</button>
+	
+	// 		<button onClick={()=> setCount (count - 1)}>
+	// 			Hazme click para disminuir
+	// 		</button>
+	// 	</div>
+	
+	// )
